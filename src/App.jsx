e5df99a134df99743b23142e023ad94e81ad92ba
hmoh24 <hamsam0097@gmail.com>
2026-07-27 20:05:54 +0100

    Basic React App setup, remove default App content, add empty Homepage component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./Home";
+
+function App() {
+  return (
+    <>
+      <p>Sculptures for sale!</p>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <p>Ancient Roman Sculptures</p>
+    </>
+  );
+}
+
+export default Home;
